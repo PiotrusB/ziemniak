462dd62ea2f50c21c4aff1314c136495d25f6488
PiotrusB <piotrpawelbozek@gmail.com>
2025-05-10 22:09:58 +0200

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let potato = [];
+function preload() {
+for(let i = 0; i < 9; i++) {
+potato[i] = loadImage("ziemniaki/potato" + String(i) + ".jpg");
+}
+}
+
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background(0);
+image(potato[int(random(9))], 0, 0, width, height);
+}
+
+function draw() {
+
+}
