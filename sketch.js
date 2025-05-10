d4a34abe247237379ce86620eab2af930e1f8ea3
PiotrusB <piotrpawelbozek@gmail.com>
2025-05-10 23:03:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let potato = [];
+let index = 0;
 function preload() {
 potato[0] = loadImage('potato0.png');
 potato[1] = loadImage('potato1.png');
@@ -9,14 +10,25 @@ potato[5] = loadImage('potato5.jpg');
 potato[6] = loadImage('potato6.jpg');
 potato[7] = loadImage('potato7.jpg');
 potato[8] = loadImage('potato8.jpg');
+for(let i = 0; i < 9; i++) {
+potato[i].resize(windowWidth, 0);
+}
 }
 
 function setup() {
 createCanvas(windowWidth, windowHeight);
-background(0, 0, 0);
-image(potato[int(random(0, 8))], 0, 0);
 }
 
 function draw() {
-
+if(mousePressed) {
+let newIndex = random(0, 8);
+while(1) {
+if(newIndex != index) {break;}
+else {
+newIndex = random(0, 8);
+}
+}
+while(mousePressed);
+}
+image(potato[index], 0, 0);
 }
