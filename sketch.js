0bbfc3a5de065c56dc1de9c882fbaaba1ba5c1c6
PiotrusB <piotrpawelbozek@gmail.com>
2025-05-10 22:55:54 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ potato[8] = loadImage('potato8.jpg');
 }
 
 function setup() {
-createCanvas(1000, 1000);
+createCanvas(windowWidth, windowHeight);
 background(0, 0, 0);
 image(potato[int(random(0, 8))], 0, 0);
 }
