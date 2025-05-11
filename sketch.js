03a996402ae93fadd1a01c67416109e6f965b859
PiotrusB <piotrpawelbozek@gmail.com>
2025-05-11 09:55:30 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,9 @@ let potato = [];
 let index = 0;
 let lastChange = 0;
 function preload() {
-potato[0] = loadImage('potato0.png');
-potato[1] = loadImage('potato1.png');
-potato[2] = loadImage('potato2.jpg');
-potato[3] = loadImage('potato3.jpg');
-potato[4] = loadImage('potato4.jpg');
-potato[5] = loadImage('potato5.jpg');
-potato[6] = loadImage('potato6.jpg');
-potato[7] = loadImage('potato7.jpg');
-potato[8] = loadImage('potato8.jpg');
+for(let i = 0; i < 9; i++) {
+potato[i] = loadImage('ziemniaki/potato' + String(i) + '.png');
+}
 }
 
 function setup() {
@@ -32,8 +26,6 @@ textSize(50);
 
 function draw() {
 background(0, 10, 150);
-fill(255, 255, 255);
-rect(50, 100, windowWidth-100, windowHeight-100);
 fill(0, 200, 200);
 text("ziemniak", windowWidth/2, 50);
 image(potato[index], 50, 100);
